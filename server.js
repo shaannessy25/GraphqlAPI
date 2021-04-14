99840df1528d1fdfc01b6ed9901f9e27e6fcab06
shaannessy25 <shaan.hurley25@gmail.com>
2021-04-14 02:33:33 -0700

    finished challenge 3-7

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const {  buildSchema } = require('graphql')
+const fetch = require('node-fetch')
+require('dotenv').config()
+
 
 
 const schema = buildSchema(`
-# schema here
-type Test {
-    message: String!
-}
+    type Weather {
+        temperature: Float!
+        description: String!
+    }
+
+    type Query {
+        getWeather(zip: Int!): Weather!
+    }
 `)
 
 
 const root = {
-    // resolvers here
-}
-
+    getWeather: async ({ zip }) => {
+          const apikey = process.env.OPENWEATHERMAP_API_KEY
+          const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}`
+          const res = await fetch(url)
+          const json = await res.json()
+          const temperature = json.main.temp
+          const description = json.weather[0].description
+          return { temperature, description }
+      }
+  }
+  
 
 
 const app = express()
@@ -30,7 +45,6 @@ app.use('/graphql', graphqlHTTP({
 
 
 
-  // Start this app
 const port = 4000
 app.listen(port, () => {
   console.log('Running on port:'+port)
